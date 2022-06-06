7c1959a8dd43a0591c40a79f7daff8e33661c068
Karim <selihkarim@gmail.com>
2022-06-06 21:30:54 +0100

    [ks1020] - setup initial config for gunicorn

--- lecture-feedback/api/api.py
+++ lecture-feedback/api/api.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+CORS(app)
+
+@app.route("/")
+@cross_origin()
+def index():
+    return app.send_static_file("index.html")
 
 @app.route("/test")
+@cross_origin()
 def test():
     return {"test":"hello there sir"}
+
+if __name__ == "__main__":
+    app.run()
