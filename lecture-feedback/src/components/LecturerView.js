5d64cfbe2b156a001d8bd2e2c7ca26542584f4b5
Karim <selihkarim@gmail.com>
2022-06-09 10:46:31 +0100

    [ks1020] - removed typo

--- lecture-feedback/src/components/LecturerView.js
+++ lecture-feedback/src/components/LecturerView.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { socket, SocketContext } from "../context/socket";
 import { SocketCounter } from "./SocketCounter";
 import { Link } from "react-router-dom"
-import { Button} from '@chakra-ui/react'
 
 import {
   ChakraProvider,
