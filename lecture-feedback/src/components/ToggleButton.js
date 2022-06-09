3e0666354c8515d7e975f4a03160ac5c4515702f
SzymonKubica <szymo.kubica@gmail.com>
2022-06-09 08:52:10 +0100

    Add correct capitalisation to buttons.

--- lecture-feedback/src/components/ToggleButton.js
+++ lecture-feedback/src/components/ToggleButton.js
@@ -3,13 +3,13 @@ import { Button } from '@chakra-ui/react'
 import { socket } from "../context/socket";
 
 export const ToggleButton = (props) => {
-    const [isToggled, setState] = useState(true);
+    const [isToggled, setState] = useState(false);
 
 
 
     function handleButton() {
         setState(s => !s);
-        if (isToggled) {
+        if (!isToggled) {
             socket.emit(props.reaction);
         } else {
             socket.emit("no longer " + props.reaction)
@@ -18,7 +18,11 @@ export const ToggleButton = (props) => {
 
     return (
     <div>
-        <Button colorScheme='blue' onClick={handleButton}>{isToggled? "" : "Not "} {props.reaction}</Button>
+        {props.reaction !== "good" ? 
+        <Button colorScheme='blue' onClick={handleButton} style={{textTransform: "capitalize"}}>{isToggled? "" : "Not "} {props.reaction}</Button>
+        :
+        <Button colorScheme='blue' onClick={handleButton}>{isToggled? "Very Good" : "Normal Good"}</Button>
+        }
     </div>
     );
 }
