e3b6bef6449948ab983854697198189c67a62724
Karim <selihkarim@gmail.com>
2022-06-13 20:52:25 +0100

    [ks1020] - added function in reaction frontend to map reaction to colour

--- lecture-feedback/src/components/Reactions.js
+++ lecture-feedback/src/components/Reactions.js
@@ -11,4 +11,17 @@ export const getString = reaction => {
   return Object.values(Reaction)[Object.values(Reaction).indexOf(reaction)]
 }
 
+export function getColour(reaction) {
+  switch(reaction) {
+    case Reaction.GOOD:
+      return "green"
+    case Reaction.CONFUSED:
+      return "red"
+    case Reaction.TOO_FAST:
+      return "orange"
+    case Reaction.CHILLING:
+      return "#1da1f2" // twitter
+    }
+}
+
 export default Reaction
