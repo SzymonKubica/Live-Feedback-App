74846d6fc3e085c2f9cdd69b73952c66fae76395
Konstantinos Koupepas <kk1219@ic.ac.uk>
2022-06-23 18:59:35 +0100

    offset innit

--- lecture-feedback/src/components/Teacher/PastAnalysis/AnalysisView.js
+++ lecture-feedback/src/components/Teacher/PastAnalysis/AnalysisView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { ChakraProvider, theme, VStack, Heading, Stack, Button, Center, Input, Box, Container, Flex, Grid, GridItem, Spacer} from "@chakra-ui/react"
+import { ChakraProvider, theme, VStack, Heading, Stack, Button, Center, Input, Box, Container, Flex, Grid, GridItem, Spacer, Textarea, HStack, Text, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from "@chakra-ui/react"
 import LectureAnalysisGraph from "../LectureAnalysisGraph"
 import { useViewport } from "../../../hooks/useViewport"
 import Messages from "../Messages"
@@ -7,7 +7,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import Header from "../../Header"
 
 
-export const AnalysisView = ({isAuth, setAuth}) => {
+export const AnalysisView = ({ isAuth, setAuth }) => {
 
 
   const playerRef = React.useRef();
@@ -15,18 +15,23 @@ export const AnalysisView = ({isAuth, setAuth}) => {
 
   const { width, height } = useViewport()
 
-  let {code} = useParams()
+  let { code } = useParams()
 
-  const [time, setTime] = useState(0)
+  const [time, setTime_] = useState(0)
 
   const [comments, setComments] = useState([{ comment: "hello", reaction: "chilling" }])
   const [startTime, setStartTime] = useState(0)
   const [link, setLink] = useState("")
+  const [offset, setOffset] = useState(0)
 
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ "room": code }),
+  }
+
+  function setTime(t) {
+    setTime_(t + offset)
   }
 
   useEffect(() => {
@@ -36,66 +41,76 @@ export const AnalysisView = ({isAuth, setAuth}) => {
         setComments(data.comments)
       })
 
-      fetch('/api/get-start-time', requestOptions)
+    fetch('/api/get-start-time', requestOptions)
       .then(res => res.json())
       .then(data => {
         setStartTime(data.time)
       })
 
     fetch('/api/get-video-link', requestOptions)
-    .then(res => res.json())
-    .then(data => {
-      setLink(data.link)
-    })
+      .then(res => res.json())
+      .then(data => {
+        setLink(data.link)
+      })
   }, [])
 
   //https://pro.panopto.com/Panopto/Pages/Embed.aspx?tid=ac005dfe-14fb-47f6-9ccc-aebb00a778ee
 
   const handleAddLink = () => {
     let link = prompt("Enter Panopto Link")
-    link = link.replace("Viewer","Embed")
+    link = link.replace("Viewer", "Embed")
     setTime(0)
     setLink(link)
 
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ "room": code , "link":link}),
+      body: JSON.stringify({ "room": code, "link": link }),
     }
     fetch('/api/set-video-link', requestOptions)
-    .then(res => res.json())
-    .then(data => {
-    })
+      .then(res => res.json())
+      .then(data => {
+      })
 
   }
 
   return (
     <ChakraProvider theme={theme}>
       {/* <Flex> */}
-        <Header isAuth={isAuth} setAuth={setAuth} />
-        <Grid templateColumns="repeat(3, 1fr)" height="calc(70vh)">
-          <GridItem rowSpan={2} colSpan={2}>
-            <Container maxW={width * 0.55} maxH={height * 0.70}>
-              {link === "" ? 
+      <Header isAuth={isAuth} setAuth={setAuth} />
+      <Grid templateColumns="repeat(3, 1fr)" height="calc(70vh)">
+        <GridItem rowSpan={2} colSpan={2}>
+          <Container maxW={width * 0.55} maxH={height * 0.70}>
+            {link === "" ?
               <Box height={height * 0.65} width={width * 0.55}>
                 <Heading>No Video Saved</Heading>
                 <Button onClick={handleAddLink}>Add Panopto Link</Button>
               </Box>
               :
               <iframe src={`${link}&autoplay=true&offerviewer=true&showtitle=true&showbrand=true&captions=false&start=${time}&interactivity=all`} height={height * 0.65} width={width * 0.55} allowfullscreen allow="autoplay"></iframe>
-              }
-              <LectureAnalysisGraph room={code} setTime={setTime} />
-            </Container>
-          </GridItem>
-          <Spacer />
+            }
+            <LectureAnalysisGraph room={code} setTime={setTime} />
+            <HStack>
+              <Text >Offset (seconds): </Text>
+              <NumberInput value={offset} width='auto' onChange={({ val }) => setOffset(val)}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </HStack>
+          </Container>
+        </GridItem>
+        <Spacer />
 
-          <GridItem rowSpan={2}>
+        <GridItem rowSpan={2}>
           <Flex w="100%" h="100%" justify="center" align="center">
             <Flex w={["100%", "100%", "40%"]} h="90%" flexDir="column">
-              <Messages h="calc(40vh)" messages={comments} startTime={startTime} setTime={setTime}/>
+              <Messages h="calc(40vh)" messages={comments} startTime={startTime} setTime={setTime} />
             </Flex>
           </Flex>
-          </GridItem>
+        </GridItem>
       </Grid>
       {/* </Flex> */}
     </ChakraProvider>
